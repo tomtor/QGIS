b047d6430092cdb48f031e22dc750b5324d61f60
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-12-06 10:55:08 +0100

    Revert "Add QgsDataItem::Fast flag for QgsPGLayerItem"
    This reverts commit 9d2d9f56d1c81d42c5e85cd0b944fe07748e29bd.

--- src/providers/postgres/qgspostgresdataitems.cpp
+++ src/providers/postgres/qgspostgresdataitems.cpp
@@ -314,7 +314,7 @@ QgsPGLayerItem::QgsPGLayerItem( QgsDataItem *parent, const QString &name, const 
   : QgsLayerItem( parent, name, path, QString(), layerType, layerProperty.isRaster ? QStringLiteral( "postgresraster" ) : QStringLiteral( "postgres" ) )
   , mLayerProperty( layerProperty )
 {
-  mCapabilities |= Delete | Fertile | Fast;
+  mCapabilities |= Delete | Fertile;
   mUri = createUri();
   // No rasters for now
   setState( layerProperty.isRaster ? Populated : NotPopulated );
